9cd5a6f9b2b3adf0b35073f3b0d312ae7c9986a0
vaishnavibhedurkar1-afk <vaishnavibhedurkar1@gmail.com>
2026-07-15 22:30:08 +0530

    Updated API URL to Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected");
